fad05b68b8bcf637622c00f1f8492a4b7b0ee877
Adam Zálešák <mail@adamzalesak.cz>
2022-01-05 16:22:48 +0100

    Create Frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,42 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+        html! {
+            <div class="bg-sky-300 p-3">
+                <button class="pd-2 bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg" onclick={link.callback(|_| Msg::AddOne)}>
+                    { self.value }
+                </button>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
