c8fb548dd7b40661534c3522d2b0d62a561bd060
tomas.burianek <xburian@mail.muni.cz>
2022-01-23 18:51:52 +0100

    Add bets streaming

--- backend/src/grpc_handlers/bet_handler.rs
+++ backend/src/grpc_handlers/bet_handler.rs
@@ -2,9 +2,12 @@ use tonic::{Request, Response, Status};
 
 use crate::bet::bet_service_server::BetService;
 use crate::bet::{
-    CreateBetReply, CreateBetRequest, DeleteBetRequest, ListBetsReply, ListBetsRequest,
-    ListTicketBetsRequest,
+    Bet, CreateBetReply, CreateBetRequest, DeleteBetReply, DeleteBetRequest, ListBetsReply,
+    ListBetsRequest, ListTicketBetsRequest, StreamBetsRequest,
 };
+
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
 
 pub struct MyBetService {}
 
@@ -13,6 +16,8 @@ impl MyBetService {
         MyBetService {}
     }
 }
+
+type ResponseStream = ReceiverStream<Result<Bet, Status>>;
 
 #[tonic::async_trait]
 impl BetService for MyBetService {
@@ -36,10 +41,40 @@ impl BetService for MyBetService {
         Ok(Response::new(reply))
     }
 
-    async fn delete_bet(&self, request: Request<DeleteBetRequest>) -> Result<Response<()>, Status> {
+    async fn delete_bet(
+        &self,
+        request: Request<DeleteBetRequest>,
+    ) -> Result<Response<DeleteBetReply>, Status> {
         println!("[Server] Request from client: {:?}", &request);
 
-        Ok(Response::new(()))
+        Ok(Response::new(DeleteBetReply {}))
+    }
+
+    type StreamBetsStream = ResponseStream;
+
+    async fn stream_bets(
+        &self,
+        request: Request<StreamBetsRequest>,
+    ) -> Result<Response<Self::StreamBetsStream>, Status> {
+        println!("Client connected from: {:?}", request.remote_addr());
+
+        let (tx, rx) = mpsc::channel(4);
+
+        let bets: Vec<Bet> = vec![Bet {
+            id: 1,
+            match_id: 1,
+            rate: 1.0,
+            team_id: 1,
+            ticket_id: 1,
+        }];
+
+        tokio::spawn(async move {
+            for bet in &bets[..] {
+                tx.send(Ok(bet.clone())).await.unwrap();
+            }
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
     }
 
     async fn list_ticket_bets(
